d9c063e79e9cf5df58c878ba86db6d0c39bb7f1a
pvpstory <koliyayyt@gmail.com>
2024-05-08 20:59:15 +0200

    player class, game class start

--- main.cpp
+++ main.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class Player {
+
+public:
+    Player(string name) : name(name) {
+        int lb = 200;
+        int ub = 1000;
+        money = (rand() % (ub - lb + 1)) + lb;
+    }
+
+    void changeMoney(int amount){
+        money = money + amount;
+    }
+
+    float getMoney(){
+        return money;
+    }
+
+
+private:
+    string name;
+    float money;
+};
+
+class Game{
+public:
+    void resultGame(int amount,Player player){
+        if (amount > 0)
+            cout << "Congratulations you won: " << amount << " dollars" << endl;
+
+        else if(amount < 0)
+            cout << "Unfortunately you lost" << -amount << " dollars" << endl;
+        else
+            cout <<"You didn't win or lose" << endl;
+
+        player.changeMoney(amount);
+        cout << "Your balance now is: " << player.getMoney() << endl;
+    }
+
+
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    cout << "Hello, Brother!, what's your name" << endl;
+    string name;
+    cin >> name;
+
+    Player player(name);
+
+
+
+    return 1;
 }
+
+
