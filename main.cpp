bdc83b6a4380cd0efc0acfe70172e6d4dac301de
pvpstory <koliyayyt@gmail.com>
2024-05-13 13:11:50 +0200

    almost finished blackjack but can be improved

--- main.cpp
+++ main.cpp
@@ -68,12 +68,69 @@ class BlackJack: public Game{
             // 11 - Jack, 12 - Queen, 13 - King, 14 - Ace
 
             int dealer_hand[2];
-            int player_hand[5];
+            int player_hand[5] = {0,0,0,0,0};
 
             int bet;
             bet = make_bet();
 
             cout << ">>>Starting the round" << endl;
+            cout << ">>>Dealer gives you a card and himself a card" << endl;
+
+            player_hand[0] = random_card(deck);
+            cout << "Your hand: " << player_hand[0] << endl;
+
+            dealer_hand[0] = random_card(deck);
+            cout << "Dealer's hand: " << dealer_hand[0] << endl;
+
+            cout << ">>>Dealer gives you a card and himself a card" << endl;
+
+            player_hand[1] = random_card(deck);
+            cout << "Your hand: " << player_hand[0] << " + " <<player_hand[1] << endl;
+
+            dealer_hand[1] = random_card(deck);
+            cout << "Dealer's hand: " << dealer_hand[0] << " + " <<"Hidden" << endl;
+
+            int sum_hand = player_hand[0] + player_hand[1] + player_hand[2] + player_hand[3] + player_hand[4];
+
+            int current_index = 1;
+            while(sum_hand < 21){
+                current_index++;
+                char answer;
+                cout << "Would you like another card?: Y/N" << endl;
+                cin >> answer;
+
+                if (answer == 'N'){
+                    break;
+                }
+                player_hand[current_index] = random_card(deck);
+                int sum_hand = player_hand[0] + player_hand[1] + player_hand[2] + player_hand[3] + player_hand[4];
+                cout << "Your hand: " << player_hand[0];
+                for(int i = 1; i <= current_index;i++){
+                    cout << " + ";
+                    cout << player_hand[i];
+
+                }
+                cout << endl;
+            }
+            cout << ">>>Results" << endl;
+            cout << "Dealer's hand: " << dealer_hand[0] << " + " <<dealer_hand[1] << endl;
+            int sum_hand2 = dealer_hand[0] + dealer_hand[1];
+
+            if (sum_hand2 > 21)
+                cout << "Congratulations you won" << endl;
+            if (sum_hand2 > 21 or sum_hand == 21 or sum_hand > sum_hand2 ){
+                cout << "Congratulations you won" << endl;
+                // implent the wins
+            }
+            else
+                "Your lose"
+
+
+
+
+
+
+
 
 
 
@@ -88,6 +145,14 @@ class BlackJack: public Game{
 
         card = deck[deck_index];
         deck[deck_index] = 0;
+        if(card = 14)
+            /////////////////implemnt
+            return 0;
+
+        if (card > 10){
+            card = 10;
+        }
+
         return card;
     }
 
